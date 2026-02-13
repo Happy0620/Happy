057c7152848e96dacc6a0de37ccab948781fcd48
Happy0620 <greenzebra0123@gmail.com>
2026-02-13 16:50:02 +0545

    go go gogit add .

---
+++ Galaxy-love-main/three/examples/jsm/geometries/TextGeometry.js
@@ -0,0 +1,42 @@
+
+
+import {
+	ExtrudeGeometry
+} from 'three';
+
+class TextGeometry extends ExtrudeGeometry {
+
+	constructor( text, parameters = {} ) {
+
+		const font = parameters.font;
+
+		if ( font === undefined ) {
+
+			super(); 
+
+		} else {
+
+			const shapes = font.generateShapes( text, parameters.size );
+
+			
+
+			parameters.depth = parameters.height !== undefined ? parameters.height : 50;
+
+		
+
+			if ( parameters.bevelThickness === undefined ) parameters.bevelThickness = 10;
+			if ( parameters.bevelSize === undefined ) parameters.bevelSize = 8;
+			if ( parameters.bevelEnabled === undefined ) parameters.bevelEnabled = false;
+
+			super( shapes, parameters );
+
+		}
+
+		this.type = 'TextGeometry';
+
+	}
+
+}
+
+
+export { TextGeometry };
